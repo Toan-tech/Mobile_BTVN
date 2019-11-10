a4e40dad459d140a8b7c9a64d2e954d134544d8e
Toan-tech <toan-tech@Toan-techs-MacBook-Pro.local>
2019-11-10 16:51:18 +0700

    Bài giao diện

---
+++ Buoi11/Drop_Bounce Ball/Drop_Bounce Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Drop_Bounce Ball
+//
+//  Created by Toan-tech on 11/10/19.
+//  Copyright © 2019 Toan-tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

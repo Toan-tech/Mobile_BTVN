6353c2cff94a67aefda82b5a180cc64305aa11f7
Toan-tech <toan-tech@Toan-techs-MacBook-Pro.local>
2019-11-25 22:30:11 +0700

    Bài tập buổi 13

---
+++ Buoi13/Giaodien_AutoLayout/Giaodien_AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Giaodien_AutoLayout
+//
+//  Created by Toan-tech on 11/25/19.
+//  Copyright © 2019 Toan-tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
